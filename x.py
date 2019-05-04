be680f72a2ac7e8c3382652bed05d3c299b33b5a
aushine <1615296610@qq.com>
2019-05-04 20:59:31 +0800

    python cookbook day5_

--- x.py
+++ x.py
@@ -13,7 +13,6 @@ def out(any):
 def main():
     url_text = urllib.request.urlopen("https://search.bilibili.com/photo?keyword=%E5%85%AB%E9%87%8D%E6%A8%B1")
     url_content = url_text.read()
-    print(url_content)
 
 
 if __name__ == "__main__":
