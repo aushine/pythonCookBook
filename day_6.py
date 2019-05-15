e0514385ca12a1156f619e0e595323c080d95b4b
aushine <1615296610@qq.com>
2019-05-15 11:03:22 +0800

    python cookbook day5_

--- day_6.py
+++ day_6.py
@@ -1,13 +1,38 @@
+from sys import stdout
+
+
+def out(Any):
+    stdout.write(Any)
+
 
 def main():
     # 闭包结构
-    def lin_1(k,b):
+    def lin_1(k, b):
         def create_y(x):
             print(k * x + b)
         # 此处返回的是内部函数的引用
         return create_y
 
-    lin_1(1, 2)
+    def set_func(func):
+        def call_func():
+            out("这是权限验证1______\n")
+            out("这是权限验证2______\n")
+            func()
+        return call_func
+
+    def test1():
+        out("这是test1\n")
+
+    test1 = set_func(test1)
+    test1()
+
+    out("==========================\n")
+
+    @ set_func
+    def test2():
+        out("这是test2\n")
+
+    test2()
 
 
 if __name__ == "__main__":
