f74a33a31e2876218dc9e26eb0819d1044d96bcf
aushine <1615296610@qq.com>
2019-05-15 15:07:05 +0800

    python cookbook day5_

--- day_6.py
+++ day_6.py
@@ -29,7 +29,7 @@ def main():
     out("==========================\n")
 
     # 对有参数无返回值的函数进行装饰
-    @ set_func  # 等价于test2 = set_func(test2)
+    @set_func  # 等价于test2 = set_func(test2)
     def test2():
         out("这是test2\n")
 
@@ -45,7 +45,7 @@ def main():
         return call_func
     out("==========================\n")
 
-    @ set_func
+    @set_func
     def test3(num):
         out("这是test3,我有一个参数{}\n".format(num))
 
@@ -56,10 +56,10 @@ def main():
         def call_func(*args, **kwargs):
             out("这是权限验证1\n")
             out("这是权限验证2\n")
-            func(*args, **kwargs)
+            func(*args, **kwargs)  # 如果只是把接收的的实参名填在这个位置中, 这里传过去的值就是一个字典和一个字典.
         return call_func
 
-    @ set_func
+    @set_func
     def test4(num, *args, **kwargs):
         out("test4______%d\n" %num)
         print("test4______", args)
@@ -68,6 +68,30 @@ def main():
     test4(100)
     test4(100, 200)
     test4(100, 200, 300, m=100)
+
+    # 多个装饰器对函数进行装饰
+    def add_y(func):
+        out("进行装饰权限1的功能\n")
+
+        def call_func(*args, **kwargs):
+            out("这是权限验证1\n")
+            func(*args, **kwargs)  # 如果只是把接收的的实参名填在这个位置中, 这里传过去的值就是一个字典和一个字典.
+        return call_func
+
+    def add_x(func):
+        out("进行装饰权限2的功能\n")
+
+        def call_func(*args, **kwargs):
+            out("这是权限验证2\n")
+            out("=========>test3000<======\n")
+            func(*args, **kwargs)  # 如果只是把接收的的实参名填在这个位置中, 这里传过去的值就是一个字典和一个字典.
+        return call_func
+
+    # 对一个函数有多个装饰
+    @ add_x  # 等价于 test100 = add_x(add_y(test100))
+    @ add_y  # 等价于 test100 == add_y(test100)
+    def test100():
+        out("=======test100=======\n")
 
 
 if __name__ == "__main__":
