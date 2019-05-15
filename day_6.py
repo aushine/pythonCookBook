325522e06116d7a9a9c1495f22166a8f925e0f96
aushine <1615296610@qq.com>
2019-05-15 11:22:49 +0800

    python cookbook day5_

--- day_6.py
+++ day_6.py
@@ -28,13 +28,26 @@ def main():
 
     out("==========================\n")
 
+    # 对有参数无返回值的函数进行装饰
     @ set_func  # 等价于test2 = set_func(test2)
     def test2():
         out("这是test2\n")
 
     test2()
 
-    # 对有参数无返回值的函数进行装饰
+    # 对有一个参数的函数进行装饰
+    def set_func(func):
+        def call_func(a):
+            out("权限验证1\n")
+            out("权限验证2\n")
+            func(a)
+        return call_func
+
+    @ set_func
+    def test3(num):
+        out("这是test3,我有一个参数{}\n".format(num))
+
+    test3(100)
 
 
 if __name__ == "__main__":
